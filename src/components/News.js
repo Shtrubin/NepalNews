ddcd38574b8fdf2a6f2691dcda728471da1bfaf5
Shtrubin <rubeensht17@gmail.com>
2023-09-02 19:31:59 -0700

    adding different catgories like business, sports etc

--- src/components/News.js
+++ src/components/News.js
@@ -2,8 +2,19 @@
 import React, { Component } from 'react'
 import NewsItem from './NewsItem'
 import Spinner from './Spinner';
+import PropTypes from 'prop-types';
 
 export class News extends Component {
+static defaultProps={
+  country: 'in',
+  pageSize: 8,
+  category:'general'
+}
+static propTypes={
+  country: PropTypes.string,
+  pageSize: PropTypes.number,
+  category: PropTypes.string
+}
 
 constructor(){
   super();
@@ -14,7 +25,7 @@ constructor(){
   }
 }
   async componentDidMount(){
-    let url=`https://newsapi.org/v2/top-headlines?country=in&category=business&apiKey=30d7cd5ac32b44b985ba64030d07e05c&page=1&pageSize=${this.props.pageSize}`;
+    let url=`https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=30d7cd5ac32b44b985ba64030d07e05c&page=1&pageSize=${this.props.pageSize}`;
     this.setState({ loading: true });
     let data= await fetch(url);
     let parsedData=await data.json()
@@ -24,7 +35,7 @@ constructor(){
   }
   handlePreClick=async()=>{
     console.log('This is prev')
-    let url=`https://newsapi.org/v2/top-headlines?country=in&category=business&apiKey=30d7cd5ac32b44b985ba64030d07e05c&page=${this.state.page-1}&pageSize=${this.props.pageSize}`;
+    let url=`https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=30d7cd5ac32b44b985ba64030d07e05c&page=${this.state.page-1}&pageSize=${this.props.pageSize}`;
     this.setState({ loading: true });
     let data= await fetch(url);
     let parsedData=await data.json()
@@ -39,7 +50,7 @@ constructor(){
   handleNextClick=async()=>{
     console.log('This is next');
     if(!(this.state.page+1 > Math.ceil(this.state.totalResults/this.props.pageSize))){
-    let url=`https://newsapi.org/v2/top-headlines?country=in&category=business&apiKey=30d7cd5ac32b44b985ba64030d07e05c&page=${this.state.page+1}&pageSize=${this.props.pageSize}`;
+    let url=`https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=30d7cd5ac32b44b985ba64030d07e05c&page=${this.state.page+1}&pageSize=${this.props.pageSize}`;
     this.setState({ loading: true });
     let data= await fetch(url);
     let parsedData=await data.json()
@@ -56,11 +67,11 @@ constructor(){
     return (
 
       <div className='container my-3'>
-        <h1 className="text-center">
+        <h1 className="text-center" style={{margin: '35px 0px'}}>
         Nepal Time - A news Portal</h1>
         { this.state.loading && <Spinner/>}
         <div className="row">
-        {!this.state.loading && this.state.articles.map((element)=>{
+        {!this.state.loading && this.state.articles.map((element) =>{
         return <div className="col-md-4" key={element.url}>
           <NewsItem title={element.title?element.title.slice(0,50): " "} description={element.description?element.description.slice(0,88): ""} imageUrl={element.urlToImage} newsUrl={element.url}/>
           </div>
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -6,7 +6,7 @@ export class NewsItem extends Component {
     let {title,description,imageUrl,newsUrl} = this.props;
     return (
       <div className=''>
-      <div className="card" style={{width: "18rem"}}>
+      <div className="card">
           <img src={!imageUrl?"https://i.sozcucdn.com/wp-content/uploads/2023/08/27/elonmusk-shutter.jpeg?w=1200&h=675&mode=crop":imageUrl} className="card-img-top" alt="..."/>
           <div className="card-body">
             <h5 className="card-title">{title}</h5>
